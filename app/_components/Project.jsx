2fd69acc68852ee998ca20207c052a6a5b49bbfb
r2hu1 <newasram86@gmail.com>
2024-01-10 13:01:35 +0530

    #1

--- app/_components/Project.jsx
+++ app/_components/Project.jsx
@@ -6,10 +6,10 @@ import Link from "next/link";
 
 export default function Project({ title, alt, image, description, url }) {
     return (
-        <div className="border bg-background dark:bg-secondary max-w-fit rounded-md grid place-content-center transition">
+        <div className="border bg-background dark:bg-secondary max-w-fit rounded-md grid place-content-center mx-4">
             <div className="m-2 rounded-md relative border">
                 <span className="absolute text-xs bg-primary text-white p-2 px-3 rounded-md bottom-3 left-3">{alt}</span>
-                <img src={image} alt={title} className="max-w-[350px] h-[300px] rounded-md" />
+                <img src={image} alt={title} className="w-full h-full max-w-[350px] max-h-[300px] rounded-md" />
             </div>
             <div className="flex items-center justify-between px-6 py-4 -mt-1">
                 <h1 className="text-lg">{title}</h1>
