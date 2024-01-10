b72b0a8686587950630d4de617cfb2105862bdb0
r2hu1 <newasram86@gmail.com>
2024-01-10 23:10:25 +0530

    [tweaks]

--- app/_components/Project.jsx
+++ app/_components/Project.jsx
@@ -7,11 +7,11 @@ import Link from "next/link";
 export default function Project({ title, alt, image, description, url }) {
     return (
         <div className="relative border bg-background dark:bg-secondary max-w-fit rounded-md grid place-content-center">
-            <img className="w-full max-w-[300px] m-1 rounded-md aspect-video" src={image} alt={alt} />
-            <div className="absolute bottom-[73px] left-3 grid gap-2">
+            <img className="w-full max-w-[340px] h-full m-1 rounded-md aspect-video" src={image} alt={alt} />
+            <div className="absolute bottom-[74px] left-3 grid gap-2">
                 <span className="text-xs dark:bg-secondary bg-background p-2 px-3 rounded">{alt}</span>
             </div>
-            <div className="flex items-center justify-between px-4 p-4 -mt-[6px]">
+            <div className="flex items-center justify-between px-4 py-4 mt-[5px]">
                 <h1 className="text-lg">{title}</h1>
                 <Button size="sm">
                     <Link href={url || "/"} target="_blank" className="flex items-center gap-1 justify-center">view <ArrowUpRight className="h-4 w-4" /></Link>
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Page() {
     return(
         <div className="h-[400px] grid place-content-center">
-            <div className="border px-4 py-6 rounded-md w-[300px]">
-                <h1 className="text-3xl font-bold">404 Not Found<span className="text-primary">.</span></h1>
+            <div className="rounded-md w-[400px]">
+                <h1 className="text-3xl font-bold">4<span className="text-primary">0</span>4 Not Found<span className="text-primary">.</span></h1>
                 <p className="text-sm">This page does not exist<span className="text-primary">.</span></p>
                 <Button className="mt-4" asChild><Link href="/">Go Home</Link></Button>
             </div>
