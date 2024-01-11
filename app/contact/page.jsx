cb2e3378ff02a4a244986f846c2a1a9093340184
r2hu1 <newasram86@gmail.com>
2024-01-11 14:13:23 +0530

    [contact page]

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Page() {
-    return(
-        <div>
-            
+    return (
+        <div className="py-20">
+            <div className="text-center mb-8">
+                <h1 className="text-2xl font-bold">Contact Me<span className="text-primary">.</span></h1>
+                <p className="text-xs">I would love to hear from you<span className="text-primary">.</span></p>
+            </div>
+            <form method="post" className="max-w-md mx-auto grid gap-2 px-6">
+                <Label className="mt-3" htmlFor="name">Full Name</Label>
+                <Input autocomplete="off" id="name" type="text" placeholder="John Deo" />
+                <Label className="mt-3" htmlFor="email">Email</Label>
+                <Input autocomplete="off" id="email" type="email" placeholder="john@deo.com" />
+                <Label className="mt-3" htmlFor="message">Message</Label>
+                <Textarea className="min-h-[100px]" autocomplete="off" id="message" placeholder="I wanna hire yo.."></Textarea>
+                <Button className="mt-4" type="submit">Send</Button>
+            </form>
         </div>
     )
 }
